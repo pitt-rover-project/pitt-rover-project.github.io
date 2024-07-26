f668f17193f7b873313acd0f803f3e17268b92b8
Nick Ferguson <ndferguson@protonmail.com>
2024-07-26 15:49:57 -0400

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://pitt-soar.github.io",
-	base: "",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
