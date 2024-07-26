579490772fde0b0eb764fd110c84b53a1612b2e8
Nick Ferguson <ndferguson@protonmail.com>
2024-07-26 15:46:42 -0400

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  output: 'static',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://pitt-soar.github.io",
+	base: "website",
+	integrations: [mdx(), sitemap(), tailwind()],
 });
